f531c570612b94001edfd0e4114a0841b3e5c377
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-06-20 16:10:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+let res = [];
+
 function letterCombinations(input_digit) {
-  //Complete the function
+  let arr = [0, 1, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
+  helper(arr, input_digit, res, 0, '');
+  return res.sort();
+}
+
+function helper(arr, input_digit, res, index, str) {
+  if (str.length === input_digit.length) {
+    res.push(str);
+    return;
+  }
+  
+  if (index === input_digit.length) {
+    return;
+  }
+  
+  let match = parseInt(input_digit[index]);
+  let ss = arr[match];
+  
+  for (let i = 0; i < ss.length; i++) {
+    str = str + ss[i];
+    helper(arr, input_digit, res, index + 1, str);
+    str = str.substring(0, str.length - 1);
+  }
 }
 
 module.exports = letterCombinations;
